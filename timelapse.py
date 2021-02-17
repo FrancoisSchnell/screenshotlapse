ca9a32ad3b94745c11871d5a716331d03c743356
FrancoisSchnell <schneo@gmail.com>
2021-02-17 18:25:59 +0100

    creating date-time folder for screens

--- timelapse.py
+++ timelapse.py
@@ -1,23 +1,42 @@
-
+# imports
 import mss
 import time
+import os
 
-# variables
+# set variables here
+folderRoot = "E:/screenshots"  # set your folder (must exists)
+# folderRoot = "."  # to have it in the same folder as script
+pauseSec = 5
 monitor = 1
 record = True
 shotNumber = 1
-intervalSec = 5
+verbose = False
 
 # functions
 
 
-def takeScreenshot(mon=1):
+def createNowFolder(folderRoot):
+    """create a folder with nowd date-time as folder name"""
+    folderName = time.strftime("%Y-%m-%e-%H-%M-%S")
+    if os.path.isdir(folderName):
+        if verbose:
+            print("Folder already exists : " + folderName)
+    else:
+        print("Creating folder : " + folderName)
+        os.makedirs(folderRoot+"/"+folderName)
+    return folderName
+
+
+def takeScreenshot(shotNumber, folderName, monitor=monitor):
+    """takes screenshot with given number and optional monitor number"""
     with mss.mss() as mss_instance:
-        mss_instance.shot(mon=monitor, output=str(shotNumber)+".png")
+        mss_instance.shot(mon=monitor, output=folderRoot + "/" + folderName +
+                          "/"+str(shotNumber)+".png")
 
 
 # app
+folderName = createNowFolder(folderRoot)
 while record == True:
-    takeScreenshot()
-    time.sleep(intervalSec)
+    takeScreenshot(shotNumber, folderName)
+    time.sleep(pauseSec)
     shotNumber += 1
